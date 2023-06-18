8b3e187bf16cd37e0ed4288f51a466cafc670baa
Elena Kosiakova <Elen.Nikitina6@x5.ru>
2023-06-18 18:37:23 +0200

    fix fake data generation

--- src/test/java/ru/jiehk/tests/TestData.java
+++ src/test/java/ru/jiehk/tests/TestData.java
@@ -1,26 +1,38 @@
 package ru.jiehk.tests;
 
 import com.github.javafaker.Faker;
+import ru.jiehk.utils.RandomUtils;
+
+import java.util.Map;
 
 import static ru.jiehk.utils.RandomUtils.*;
 
 public class TestData {
 
-    static Faker faker = new Faker();
+    Faker faker = new Faker();
+    RandomUtils randomUtils = new RandomUtils();
 
-    static String firstName = faker.name().firstName(),
+    Map<String, String[]> mapStateWithCity = Map.of(
+            "NCR", new String[]{"Delhi", "Gurgaon", "Noida"},
+            "Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"},
+            "Haryana", new String[]{"Karnal", "Panipat"},
+            "Rajasthan", new String[]{"Jaipur", "Jaiselmer"}
+    );
+    String firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
             email = faker.internet().emailAddress(),
-            gender = getRandomGender(),
+            gender = faker.options().option("Male", "Female", "Other"),
             mobile = faker.phoneNumber().subscriberNumber(10),
             birthYear = String.valueOf(faker.number().numberBetween(1900, 2100)),
-            birthMonth = getRandomMonth(),
-            birthDay = getRandomDay(birthYear, birthMonth),
-            subjects = getRandomSubject(),
-            hobby = getRandomHobby(),
+            birthMonth = faker.options().option("January", "February", "March", "April", "May", "June", "July",
+                    "August", "September", "October", "November", "December"),
+            birthDay = randomUtils.getRandomDay(birthYear, birthMonth),
+            subjects = faker.options().option("Maths", "Chemistry", "Computer Science", "Commerce", "Economics",
+                    "Accounting", "Arts", "Social Studies", "History", "Civics", "Physics", "English"),
+            hobby = faker.options().option("Sports", "Reading", "Music"),
             pictureName = "test.jpeg",
             currentAddress = faker.address().fullAddress(),
-            state = getRandomState(),
-            city = getRandomCity(state);
+            state = faker.options().option(mapStateWithCity.keySet().toArray()).toString(),
+            city = faker.options().option(mapStateWithCity.get(state));
 
 }
